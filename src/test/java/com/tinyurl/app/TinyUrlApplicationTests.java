5f831532a67f2e4406e316d8aa6e5bcea7233a3b
satyam-sm <satyam101905@gmail.com>
2021-06-19 16:50:20 -0700

    sping properties

--- src/test/java/com/tinyurl/app/TinyUrlApplicationTests.java
+++ src/test/java/com/tinyurl/app/TinyUrlApplicationTests.java
@@ -1,13 +1,9 @@
 package com.tinyurl.app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TinyUrlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
